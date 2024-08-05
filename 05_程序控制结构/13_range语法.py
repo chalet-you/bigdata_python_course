89d603f9d481d1931ee2d4d016b7d804af179724
chalet-you <84378777+chalet-you@users.noreply.github.com>
2024-08-05 15:12:54 +0800

    Create 13_range语法.py

---
+++ 05_程序控制结构/13_range语法.py
@@ -0,0 +1,21 @@
+"""
+演示Python中的range()语法的基本使用
+"""
+
+for x in range(10):
+    print(x)
+
+print("===============")
+
+for x in range(5, 10):
+    print(x)
+
+print("===============")
+
+for x in range(5, 10, 2):
+    print(x)
+
+print("===============")
+
+for x in range(10):
+    print("Hello World")
