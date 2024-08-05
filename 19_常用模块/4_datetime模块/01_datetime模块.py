40a3d517815976f5d570de2d30b420a37163725a
chalet-you <84378777+chalet-you@users.noreply.github.com>
2024-08-05 15:18:18 +0800

    Update 01_datetime模块.py

--- 19_常用模块/4_datetime模块/01_datetime模块.py
+++ 19_常用模块/4_datetime模块/01_datetime模块.py
@@ -3,3 +3,32 @@
 # @Author : chalet.you
 # @File : 04_datetime模块
 # @Project : bigdata_python_course
+import datetime
+
+# 创建日期
+d = datetime.date(2024, 7, 17)
+print(d)
+
+# 创建时间
+t = datetime.time(14,25, 23)
+print(t)
+
+# 创建日期时间
+dt = datetime.datetime(2024, 7, 17, 14,25, 23)
+print(dt, type(dt))
+
+print("="*50)
+
+# 获取当前时间
+now = datetime.datetime.now()
+print(now.strftime("%Y-%m-%d %X"), type(now))
+
+# 只获取当前日期
+today = datetime.date.today()
+print(today)
+
+print("="*50)
+print(type(datetime.date.fromtimestamp(1234564525)))
+
+
+
